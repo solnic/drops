1cea56023a152fd5e06995c9220e40c75af2568e
Peter Solnica <peter@solnica.online>
2023-09-04 10:14:59 +0200

    Set version to 0.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Drops.MixProject do
   def project do
     [
       app: :drops,
-      version: "0.1.0",
+      version: "0.0.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps()
