333a372a8def80d94e6fe2134415453dc9efe665
Peter Solnica <peter@solnica.online>
2023-09-04 18:15:14 +0200

    Add super basic schema DSL

--- lib/drops.ex
+++ lib/drops.ex
@@ -1,18 +1,53 @@
 defmodule Drops do
-  @moduledoc """
-  Documentation for `Drops`.
-  """
+  defmodule Contract do
+    defmacro __using__(_opts) do
+      quote do
+        alias Drops.Predicates
 
-  @doc """
-  Hello world.
+        import Drops.DSL
 
-  ## Examples
+        def schema do
+          @schema
+        end
 
-      iex> Drops.hello()
-      :world
+        def apply(data) do
+          Enum.map(Enum.reverse(@schema), &validate(data, &1))
+        end
 
-  """
-  def hello do
-    :world
+        def validate(data, {:required, name, type}) do
+          if Map.has_key?(data, name) do
+            Predicates.type?(type, data[name])
+          else
+            {:error, {:has_key?, name}}
+          end
+        end
+      end
+    end
+  end
+
+  defmodule Predicates do
+    def type?(:string, value) when is_binary(value), do: {:ok, value}
+    def type?(:string, value), do: {:error, {:string?, value}}
+
+    def type?(:integer, value) when is_integer(value), do: {:ok, value}
+    def type?(:integer, value), do: {:error, {:integer?, value}}
+  end
+
+  defmodule DSL do
+    defmacro schema(do: body) do
+      Module.register_attribute(__CALLER__.module, :schema, accumulate: true)
+
+      quote do
+        unquote(body)
+      end
+    end
+
+    defmacro required(name, type) do
+      required(__CALLER__.module, name, type)
+    end
+
+    defp required(source, name, type) do
+      Module.put_attribute(source, :schema, {:required, name, type})
+    end
   end
 end
--- test/drops_test.exs
+++ test/drops_test.exs
@@ -1,8 +1,21 @@
 defmodule DropsTest do
   use ExUnit.Case
-  doctest Drops
 
-  test "greets the world" do
-    assert Drops.hello() == :world
+  describe "schema" do
+    test "defining a required key" do
+      defmodule TestContract do
+        use Drops.Contract
+
+        schema do
+          required(:name, :string)
+          required(:age, :integer)
+        end
+      end
+
+      assert [{:ok, "Jane"}, {:error, {:has_key?, :age}}] = TestContract.apply(%{name: "Jane"})
+
+      assert [{:ok, "Jane"}, {:ok, 21}] = TestContract.apply(%{name: "Jane", age: 21})
+      assert [{:error, {:string?, 312}}, {:ok, 21}] = TestContract.apply(%{name: 312, age: 21})
+    end
   end
 end
