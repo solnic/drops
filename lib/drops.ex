4e4d3ba5ca5509ccd04eda18b08d8c94e0674fcd
Peter Solnica <peter@solnica.online>
2023-09-04 09:21:05 +0200

    mix new

---
+++ lib/drops.ex
@@ -0,0 +1,18 @@
+defmodule Drops do
+  @moduledoc """
+  Documentation for `Drops`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Drops.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/drops_test.exs
@@ -0,0 +1,8 @@
+defmodule DropsTest do
+  use ExUnit.Case
+  doctest Drops
+
+  test "greets the world" do
+    assert Drops.hello() == :world
+  end
+end
