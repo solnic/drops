d46a8fc6a56dc8cdbe03398444a0a0712c254197
Peter Solnica <peter@valued.app>
2023-10-25 09:06:38 +0200

    Update dep on elixir to ~> 1.14

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Drops.MixProject do
     [
       app: :drops,
       version: @version,
-      elixir: "~> 1.15",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       licenses: "MIT",
